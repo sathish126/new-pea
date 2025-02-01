a96c1fe95a61ea3bb9067eeae4dcd37c98226090
sathishpandiyan126@gmail.com <sathishpandiyan126@gmail.com>
2025-02-01 22:56:23 +0530

    addlink

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -5,38 +5,38 @@ import ParticlesComponent from './particles';
 // Combined data for Production Engineering Association (PEA) and Society of Manufacturing Engineers (SME)
 const combinedMembers = [
   // Secretary
-  { name: "NITIS PRABU M", organization: "PEA", role: "Secretary", image: "/assets/img/Team/SEC/nitis_sec.jpg", linkedin: "https://linkedin.com/in/peasecretary" },
-  { name: "ARUN K", organization: "SME", role: "Secretary", image: "/assets/img/Team/SEC/arun_sec.png", linkedin: "https://linkedin.com/in/smesecetary" },
+  { name: "NITIS PRABU M", organization: "PEA", role: "Secretary", image: "/assets/img/Team/SEC/nitis_sec.jpg", linkedin: "https://www.linkedin.com/in/nitisprabu710?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+  { name: "ARUN K", organization: "SME", role: "Secretary", image: "/assets/img/Team/SEC/arun_sec.png", linkedin: "https://www.linkedin.com/in/arun-kumar-02a1771a7" },
 
   // Joint Secretaries
-  { name: "B VIKASHINI", organization: "PEA", role: "Joint Secretary", image: "/assets/img/Team/JOINT_SEC/Vikashini.JPG", linkedin: "https://linkedin.com/in/joinsecretary1" },
-  { name: "BALAJI C", organization: "PEA", role: "Joint Secretary", image: "/assets/img/Team/JOINT_SEC/balaji.jpeg", linkedin: "https://linkedin.com/in/joinsecretary2" },
-  { name: "SUBIRAMANIAN V J", organization: "SME", role: "Joint Secretary", image: "/assets/img/Team/JOINT_SEC/subiramanian.jpeg", linkedin: "https://linkedin.com/in/smejoinsecretary1" },
+  { name: "B VIKASHINI", organization: "PEA", role: "Joint Secretary", image: "/assets/img/Team/JOINT_SEC/Vikashini.JPG", linkedin: "https://www.linkedin.com/in/vikashini-balasubramanian-96b90324a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" },
+  { name: "BALAJI C", organization: "PEA", role: "Joint Secretary", image: "/assets/img/Team/JOINT_SEC/balaji.jpeg", linkedin: "https://www.linkedin.com/in/balaji-c-608b2925b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+  { name: "SUBIRAMANIAN V J", organization: "SME", role: "Joint Secretary", image: "/assets/img/Team/JOINT_SEC/subiramanian.jpeg", linkedin: "https://www.linkedin.com/in/subiramanian-v-j-878917249?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
   { name: "R S DIYA VISALI", organization: "SME", role: "Joint Secretary", image: "/assets/img/Team/JOINT_SEC/Diya.jpg", linkedin: "https://linkedin.com/in/smejoinsecretary2" },
 
   // Senior Executives
   { name: "R R DHIVAGARAN", organization: "PEA", role: "Senior Executive", image: "/assets/img/Team/SENIOR_EXE/dhivagar.JPG", linkedin: "https://linkedin.com/in/seniorexecutive1" },
-  { name: "DHINNESH S", organization: "PEA", role: "Senior Executive", image: "/assets/img/Team/SENIOR_EXE/dhinnesh.jpeg", linkedin: "https://linkedin.com/in/seniorexecutive2" },
-  { name: "SHANMUGAM S", organization: "PEA", role: "Senior Executive", image: "/assets/img/Team/SENIOR_EXE/shanmugam.jpg", linkedin: "https://linkedin.com/in/seniorexecutive3" },
+  { name: "DHINNESH S", organization: "PEA", role: "Senior Executive", image: "/assets/img/Team/SENIOR_EXE/dhinnesh.jpeg", linkedin: "https://www.linkedin.com/in/dhinnesh-s" },
+  { name: "SHANMUGAM S", organization: "PEA", role: "Senior Executive", image: "/assets/img/Team/SENIOR_EXE/shanmugam.jpg", linkedin: "https://www.linkedin.com/in/shanmugam-s-195477255?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
   { name: "J DEVI SRI", organization: "PEA", role: "Senior Executive", image: "/assets/img/Team/SENIOR_EXE/devisri.jpeg", linkedin: "https://linkedin.com/in/seniorexecutive4" },
-  { name: "NANDHAGOPAL S", organization: "SME", role: "Senior Executive", image: "/assets/img/Team/SENIOR_EXE/nandhagopal.jpg", linkedin: "https://linkedin.com/in/smeseniorexecutive1" },
-  { name: "KRUTHI T P", organization: "SME", role: "Senior Executive", image: "/assets/img/Team/SENIOR_EXE/kruthi.jpg", linkedin: "https://linkedin.com/in/smeseniorexecutive2" },
-  { name: "V JAYASENTHILNATHAN", organization: "SME", role: "Senior Executive", image: "/assets/img/Team/SENIOR_EXE/senthi.jpg", linkedin: "https://linkedin.com/in/smeseniorexecutive3" },
-  { name: "GOKUL RAMANA V", organization: "SME", role: "Senior Executive", image: "/assets/img/Team/SENIOR_EXE/gokul.JPG", linkedin: "https://linkedin.com/in/smeseniorexecutive4" },
+  { name: "NANDHAGOPAL S", organization: "SME", role: "Senior Executive", image: "/assets/img/Team/SENIOR_EXE/nandhagopal.jpg", linkedin: "www.linkedin.com/in/nandhagopal-sathasivam" },
+  { name: "KRUTHI T P", organization: "SME", role: "Senior Executive", image: "/assets/img/Team/SENIOR_EXE/kruthi.jpg", linkedin: "www.linkedin.com/in/kruthi03" },
+  { name: "V JAYASENTHILNATHAN", organization: "SME", role: "Senior Executive", image: "/assets/img/Team/SENIOR_EXE/senthi.jpg", linkedin: "https://www.linkedin.com/in/v-jayasenthil-nathan-2117k?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" },
+  { name: "GOKUL RAMANA V", organization: "SME", role: "Senior Executive", image: "/assets/img/Team/SENIOR_EXE/gokul.JPG", linkedin: "https://www.linkedin.com/in/gokul-ramana-venkatesh-7292602b8" },
 
   // Junior Executives
   { name: "SUKANTH N R", organization: "PEA", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/sukanth.jpg", linkedin: "https://linkedin.com/in/juniorexecutive1" },
   { name: "DEEPIKA S P", organization: "PEA", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/Dheepika.jpg", linkedin: "https://linkedin.com/in/juniorexecutive2" },
-  { name: "NHARGUNA NANGAI M B", organization: "PEA", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/NN.jpeg", linkedin: "https://linkedin.com/in/juniorexecutive3" },
-  { name: "NIMESHA S", organization: "PEA", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/Nimesha.jpg", linkedin: "https://linkedin.com/in/juniorexecutive4" },
-  { name: "AISHAA NIHAR Z", organization: "PEA", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/aisha.jpeg", linkedin: "https://linkedin.com/in/juniorexecutive5" },
+  { name: "NHARGUNA NANGAI M B", organization: "PEA", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/NN.jpeg", linkedin: "www.linkedin.com/in/nharguna-nangai-m-b-677b20298" },
+  { name: "NIMESHA S", organization: "PEA", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/Nimesha.jpg", linkedin: "https://www.linkedin.com/in/nimesha20067872006787?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+  { name: "AISHAA NIHAR Z", organization: "PEA", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/aisha.jpeg", linkedin: "https://www.linkedin.com/in/aishaa-nihar-z-7b83a4276?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
   { name: "KANCIA ROSLIN MARY", organization: "PEA", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/Kancia.jpg", linkedin: "https://linkedin.com/in/juniorexecutive6" },
-  { name: "RITHIKA G", organization: "SME", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/rithika.jpg", linkedin: "https://linkedin.com/in/smejuniorexecutive1" },
+  { name: "RITHIKA G", organization: "SME", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/rithika.jpg", linkedin: "hhttps://www.linkedin.com/in/rithika-govindaraj08?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
   { name: "VINOTHINI D", organization: "SME", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/Vinothini.jpg", linkedin: "https://linkedin.com/in/smejuniorexecutive2" },
-  { name: "KRISITHA J S", organization: "SME", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/Krisitha.jpg", linkedin: "https://linkedin.com/in/smejuniorexecutive3" },
-  { name: "MOHAMED VASEEM J", organization: "SME", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/vaseem.jpeg", linkedin: "https://linkedin.com/in/smejuniorexecutive4" },
-  { name: "MANJUSWETHA S V", organization: "SME", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/Manjuswetha.jpg", linkedin: "https://linkedin.com/in/smejuniorexecutive5" },
-  { name: "KISHORE KANΝΑΝ Β", organization: "SME", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/kishorekannan.jpg", linkedin: "https://linkedin.com/in/smejuniorexecutive6" },
+  { name: "KRISITHA J S", organization: "SME", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/Krisitha.jpg", linkedin: "https://www.linkedin.com/in/krisitha-j-s-b342562b3" },
+  { name: "MOHAMED VASEEM J", organization: "SME", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/vaseem.jpeg", linkedin: "https://www.linkedin.com/in/mohamed-vaseem-5bb31b28b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+  { name: "MANJUSWETHA S V", organization: "SME", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/Manjuswetha.jpg", linkedin: "https://www.linkedin.com/in/manjuswetha-s-v-b981a42b3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+  { name: "KISHORE KANΝΑΝ Β", organization: "SME", role: "Junior Executive", image: "/assets/img/Team/JUNIOR_EXE/kishorekannan.jpg", linkedin: "https://www.linkedin.com/in/kishore04k?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
 
   // Common volunteers for both organizations
   { name: "THIRUSURIYAA S U", role: "Volunteer", image: "/assets/assets/img/volunteer1.jpg", linkedin: "https://linkedin.com/in/volunteer1" },
